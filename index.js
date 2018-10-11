493aea2efc8bd22a46556193be724f268cd46555
Flatiron School <andrewsprague@outlook.com>
2018-10-11 10:12:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(() => saveLincoln(parseInt($('#interval').val())))
 
 function saveLincoln(interval) {
-  if(isNaN(interval)) {
+  if (isNaN(interval)) {
     interval = 10000
   }
-  $('#foreground').fadeOut(interval);
+  // $('#foreground').fadeOut(interval);
+  foreground = document.querySelector('#foreground');
+  fadeOut(foreground, interval);
+
 }
+
+
+function fadeOut(fadeTarget, interval) {
+  var fadeEffect = setInterval(function () {
+    if (!fadeTarget.style.opacity) {
+      fadeTarget.style.opacity = 1;
+    }
+    if (fadeTarget.style.opacity > 0) {
+      fadeTarget.style.opacity -= 0.01;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  }, interval / 100);
+}
+
+button = document.getElementById('save_lincoln');
+
+button.addEventListener('click', () => {
+  console.log('fired');
+  saveLincoln(parseInt(document.querySelector('#interval').value));
+});
